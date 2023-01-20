395d56e665b2f6d6ad3227fd956caf5b9f83a717
sunyuchao <672208496@qq.com>
2023-01-20 11:20:35 +0800

    add(ddns)1.add remove ddnsConfig.

--- src/components/ConfigMainComponent.tsx
+++ src/components/ConfigMainComponent.tsx
@@ -1,15 +1,14 @@
 import axios from "axios";
 import {ProColumns, ProTable} from "@ant-design/pro-components";
-import {DDNSConfigItem} from "@/obj/DDNSConfigItem";
-import {Button, Switch} from "antd";
+import {DDNSConfigItem, DDNSConfigKey} from "@/obj/DDNSConfigItem";
+import {Button, message, Switch} from "antd";
 import React, {Component} from "react";
 import BaseLayout from "@/pages/BaseLayout";
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:3364/api/',
-  timeout: 1000
+  timeout: 5000
 });
-
 
 export class ConfigMainComponent extends Component<any, any> {
 
@@ -34,6 +33,18 @@ export class ConfigMainComponent extends Component<any, any> {
       })
   }
 
+  removeConfig(key: DDNSConfigKey) {
+    axiosInstance.postForm("manager/ddnsConfig/remove", key)
+      .then(() => {
+        message.success('删除成功').then();
+        this.refreshTable();
+      })
+      .catch(e => {
+        message.error('删除失败').then();
+        console.log(e)
+      })
+  }
+
   render() {
     const {ddnsConfigList} = this.state;
     return (
@@ -43,7 +54,7 @@ export class ConfigMainComponent extends Component<any, any> {
             dataSource={ddnsConfigList}
             rowKey={item => item.ddnsConfigKey.domainName + '_' + item.ddnsConfigKey.domainSubName}
             pagination={{showQuickJumper: true,}}
-            columns={columns}
+            columns={this.columns}
             search={false}
             dateFormatter="string"
             headerTitle="配置列表"
@@ -56,76 +67,82 @@ export class ConfigMainComponent extends Component<any, any> {
       </BaseLayout>
     );
   }
+
+
+  // 数据列配置
+  columns: ProColumns<DDNSConfigItem>[] = [
+    {
+      title: '顶级域名',
+      width: 80,
+      key: 'ddnsConfigKey_domainName',
+      dataIndex: 'ddnsConfigKey',
+      render: (_, item) => item.ddnsConfigKey.domainName,
+      sorter: (a, b) => (a.ddnsConfigKey.domainName > b.ddnsConfigKey.domainSubName ? 1 : -1),
+    },
+    {
+      title: '二级域名',
+      width: 80,
+      key: 'ddnsConfigKey_domainSubName',
+      dataIndex: 'ddnsConfigKey',
+      render: (_, item) => item.ddnsConfigKey.domainSubName,
+    },
+    {
+      title: '服务商类型',
+      width: 80,
+      dataIndex: 'dnsServerType',
+      valueEnum: {
+        all: {text: '全部'},
+        TENCENT: {text: '腾讯云'},
+        HUAWEI: {text: '华为云'},
+        ALIYUN: {text: '阿里云'},
+      },
+    },
+    /*  {
+        title: '服务商配置',
+        width: 80,
+        dataIndex: 'dnsServerParam',
+      },*/
+    {
+      title: '记录生存时间',
+      width: 80,
+      dataIndex: 'ddnsRecordAliveTime',
+      render: (_) => _,
+    },
+
+    {
+      title: '刷新周期',
+      width: 80,
+      dataIndex: 'schedulerCron',
+      render: (_) => _,
+    },
+    {
+      title: '启动状态',
+      width: 80,
+      dataIndex: 'activate',
+      render: (_, item) => <Switch checked={item.activate}/>,
+      valueEnum: {
+        true: {text: '启动'},
+        false: {text: '停止'},
+      },
+    },
+    {
+      title: '操作',
+      width: 180,
+      key: 'option',
+      valueType: 'option',
+      render: (_, item) => [
+        //todo 待实现逻辑
+        <a key="link">修改</a>,
+        //todo 待实现逻辑
+        <a key="link2" onClick={() => {
+          this.removeConfig(item.ddnsConfigKey);
+        }
+        }>删除</a>,
+      ],
+    },
+  ];
+
+
 }
 
-
-// 数据列配置
-const columns: ProColumns<DDNSConfigItem>[] = [
-  {
-    title: '顶级域名',
-    width: 80,
-    key: 'ddnsConfigKey_domainName',
-    dataIndex: 'ddnsConfigKey',
-    render: (_, item) => item.ddnsConfigKey.domainName,
-    sorter: (a, b) => (a.ddnsConfigKey.domainName > b.ddnsConfigKey.domainSubName ? 1 : -1),
-  },
-  {
-    title: '二级域名',
-    width: 80,
-    key: 'ddnsConfigKey_domainSubName',
-    dataIndex: 'ddnsConfigKey',
-    render: (_, item) => item.ddnsConfigKey.domainSubName,
-  },
-  {
-    title: '服务商类型',
-    width: 80,
-    dataIndex: 'dnsServerType',
-    valueEnum: {
-      all: {text: '全部'},
-      TENCENT: {text: '腾讯云'},
-      HUAWEI: {text: '华为云'},
-      ALIYUN: {text: '阿里云'},
-    },
-  },
-  /*  {
-      title: '服务商配置',
-      width: 80,
-      dataIndex: 'dnsServerParam',
-    },*/
-  {
-    title: '记录生存时间',
-    width: 80,
-    dataIndex: 'ddnsRecordAliveTime',
-    render: (_) => _,
-  },
-
-  {
-    title: '刷新周期',
-    width: 80,
-    dataIndex: 'schedulerCron',
-    render: (_) => _,
-  },
-  {
-    title: '启动状态',
-    width: 80,
-    dataIndex: 'activate',
-    render: (_, item) => <Switch checked={item.activate}/>,
-    valueEnum: {
-      true: {text: '启动'},
-      false: {text: '停止'},
-    },
-  },
-  {
-    title: '操作',
-    width: 180,
-    key: 'option',
-    valueType: 'option',
-    render: () => [
-      //todo 待实现逻辑
-      <a key="link">修改</a>,
-      //todo 待实现逻辑
-      <a key="link2">删除</a>,
-    ],
-  },
-];
 
